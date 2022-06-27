95346c05c58e45951ab5459f159f9c16bd22592a
M1234543 <2985976827@qq.com>
2022-06-27 09:47:51 +0800

    数据库ip更改

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES={
         'NAME':'stock',
         'USER':'lx',
         'PASSWORD':'123456',
-        'HOST':'10.236.66.20',
+        'HOST':'10.236.66.8',
         'PORT':'3306',
     }
 }
