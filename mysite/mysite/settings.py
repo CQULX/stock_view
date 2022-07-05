0f338d4b583b47a24acb63fdbf235a4e75bf3d36
M1234543 <2985976827@qq.com>
2022-07-05 09:06:23 +0800

    ip

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES={
         'NAME':'stock',
         'USER':'lx',
         'PASSWORD':'123456',
-        'HOST':'10.236.66.44',
+        'HOST':'10.236.66.16',
         'PORT':'3306',
     }
 }
